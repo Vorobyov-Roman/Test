e31056eebe37295e8edd7649f0e0b2d86f32ddb8
Vorobyov-Roman <vorobrawr@gmail.com>
2015-06-26 19:42:46 +0300

    added a box to represent bondaries

--- main.js
+++ main.js
@@ -5,13 +5,13 @@ $(document).ready(function(){
 		y: 1
 	};
 
-	var step = 10;
+	var ratio = 10;
 
 	var bond = {
 		left: 	0,
 		up: 	0,
-		right: 	($(document).width() - $('#obj').width()) / step,
-		down: 	($(document).height() - $('#obj').height()) / step
+		right: 	($(document).width() - $('#obj').width()) / ratio,
+		down: 	($(document).height() - $('#obj').height()) / ratio
 	};
 
 	var move = {
@@ -21,9 +21,12 @@ $(document).ready(function(){
 		40: function(){ if (this.y < bond.down) this.y++; }
 	};
 
+//	$('#box').width($(document).width() * 0.9);
+//	$('#box').height($(document).height() * 0.9);
+
 	$(document).keydown(function(key){
 		move[key.which].call(obj);
 
-		$('#block').offset({ left: obj.x * step, top: obj.y * step });
+		$('#block').offset({ left: obj.x * ratio, top: obj.y * ratio });
 	});
 });
