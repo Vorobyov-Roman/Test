9de69b5565887a55592b6c2adf0cfa8d51e7d744
Vorobyov-Roman <vorobrawr@gmail.com>
2015-06-26 20:36:43 +0300

    now movement is bound to the box

--- main.js
+++ main.js
@@ -1,28 +1,32 @@
 $(document).ready(function(){
 
-	var obj = {
-		x: 1,
-		y: 1
-	};
+	var box = $('#box');
 
 	var ratio = 10;
 
+	var obj = {
+		x: 1,
+		y: 1,
+		
+		get left(){ return this.x * ratio; },
+		get top(){ return this.y * ratio; },
+		get right(){ return this.x * ratio + $('#block').width(); },
+		get bottom(){ return this.y * ratio + $('#block').height(); }
+	};
+
 	var bond = {
-		left: 	0,
-		up: 	0,
-		right: 	($(document).width() - $('#obj').width()) / ratio,
-		down: 	($(document).height() - $('#obj').height()) / ratio
+		get left(){ return box.offset().left; },
+		get top(){ return box.offset().top; },
+		get right(){ return box.width(); },
+		get bottom(){ return box.height(); }
 	};
 
 	var move = {
-		37: function(){ if (this.x > bond.left) this.x--; },
-		38: function(){ if (this.y > bond.up) this.y--; },
-		39: function(){ if (this.x < bond.right) this.x++; },
-		40: function(){ if (this.y < bond.down) this.y++; }
+		37: function(){ if (this.left - ratio > bond.left) this.x--; },
+		38: function(){ if (this.top - ratio > bond.top) this.y--; },
+		39: function(){ if (this.right < bond.right) this.x++; },
+		40: function(){	if (this.bottom < bond.bottom) this.y++; }
 	};
-
-//	$('#box').width($(document).width() * 0.9);
-//	$('#box').height($(document).height() * 0.9);
 
 	$(document).keydown(function(key){
 		move[key.which].call(obj);
