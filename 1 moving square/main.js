7b9d80a9720dc24344be613fdb0ee0d8105faad3
Vorobyov-Roman <vorobrawr@gmail.com>
2015-06-28 03:59:29 +0300

    now it moves smoothly

--- 1 moving square/main.js
+++ 1 moving square/main.js
@@ -13,23 +13,66 @@ $(document).ready(function(){
 	var box = new ObjectOnAPage('box');
 
 	var obj = {
-		x:	0,
-		y:	0
+		x: 0,
+		y: 0,
+
+		movingLeft: false,
+		movingUp: false,
+		movingRight: false,
+		movingDown: false,
 	};
 
 	var move = {
-		37:	function(){ if(block.left > box.left) this.x-- },
-		38:	function(){ if(block.top > box.top) this.y-- },
-		39:	function(){ if(block.right < box.right) this.x++ },
-		40:	function(){ if(block.bottom < box.bottom) this.y++ },
+		left:  function(){ if (block.left > box.left) this.x-- },
+		up:    function(){ if (block.top > box.top) this.y-- },
+		right: function(){ if (block.right < box.right) this.x++ },
+		down:  function(){ if (block.bottom < box.bottom) this.y++ },
 	};
 
 	$(document).keydown(function(key){
-		try {
-			move[key.which].call(obj);
-			block.object.css({ top: obj.y, left: obj.x });
-		} catch(e) {
-			//"handle" other keys
+		switch (key.which) {
+		case 37: obj.movingLeft = true;
+			break;
+		case 38: obj.movingUp = true;
+			break;
+		case 39: obj.movingRight = true;
+			break;
+		case 40: obj.movingDown = true;
+			break;
 		}
 	});
+	$(document).keyup(function(key){
+		switch (key.which) {
+		case 37: obj.movingLeft = false;
+			break;
+		case 38: obj.movingUp = false;
+			break;
+		case 39: obj.movingRight = false;
+			break;
+		case 40: obj.movingDown = false;
+			break;
+		}
+	});
+
+	var tick = function() {
+		if (obj.movingLeft) move.left.call(obj);
+		if (obj.movingUp) move.up.call(obj);
+		if (obj.movingRight) move.right.call(obj);
+		if (obj.movingDown) move.down.call(obj);
+
+		block.object.css({ top: obj.y, left: obj.x });
+
+		setTimeout(tick, 30);
+	};
+
+	tick();
+
+//	setTimeout(function(){
+//		if (obj.movingLeft) move.left.call(obj);
+//		if (obj.movingUp) move.up.call(obj);
+//		if (obj.movingRight) move.right.call(obj);
+//		if (obj.movingDown) move.down.call(obj);
+//
+//		block.object.css({ top: obj.y, left: obj.x });
+//	}, 30);
 });
