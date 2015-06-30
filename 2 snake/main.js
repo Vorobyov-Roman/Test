0bc35f51471748e2f041787ebfcbf412500c382d
Vorobyov-Roman <vorobrawr@gmail.com>
2015-06-30 23:52:39 +0300

    basic functionality without rendering
    hope it works

--- 2 snake/main.js
+++ 2 snake/main.js
@@ -1,3 +1,47 @@
 $(document).ready(function(){
-	
+	var WEST  = 0,
+	    NORTH = 1,
+	    EAST  = 2,
+	    SOUTH = 3;
+
+	function Node(){
+		this.domObj = $('#box').append('<div class="block"></div>');
+		this.next   = null;
+	};
+
+	var snake = {
+		head: null,
+		tail: null,
+
+		direction: {
+			value: null,
+
+			get(){
+				return this.value;
+			},
+			set(newVal){
+				switch (this.value){
+				case WEST:  if (newVal == EAST) return;
+				case NORTH:	if (newVal == SOUTH) return;
+				case EAST:  if (newVal == WEST) return;
+				case SOUTH: if (newVal == NORTH) return;
+				default:    break;
+				}
+
+				this.value = newVal;
+			}
+		},
+		changeDirection: function(newDir){
+			this.direction = newDir;
+		},
+		move: function(extend){
+			if (!extend){
+				this.tail.domObj.remove();
+				this.tail = this.tail.next;
+			}
+
+			this.head.next = new Node();
+			this.head = this.head.next;
+		}
+	};
 });
